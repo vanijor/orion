5f7db9806a302857520d6d5faab2be197b76acf6
Jorge Helio <vanijor@hotmail.com>
2017-07-16 17:41:02 -0300

    add ancora animada

--- js/main.js
+++ js/main.js
@@ -9,3 +9,8 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
+$("ul.navbar-nav > li > a").on('click', function(event){
+	event.preventDefault();
+	$("html, body").animate({scrollTop:$(this.hash).offset().top}, 1200);
+});
+
